699794ee77f68d7b3798a261244e550b9f0c1053
zachbrown12 <zachmb22@gmail.com>
2020-03-25 17:11:35 -0400

    3-25

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
 
     def current_user
-      User.find_by(id: session[:user_id])
+      @current_user = User.find_by(id: session[:user_id])
     end
 
     def logged_in?
--- app/controllers/tracked_meals_controller.rb
+++ app/controllers/tracked_meals_controller.rb
@@ -3,21 +3,27 @@ class TrackedMealsController < ApplicationController
 
 def index
     @tracked_meals = TrackedMeal.all
+    @tracked_meal = TrackedMeal.new
 end
 
 def new
     @tracked_meal = TrackedMeal.new
-    @user = User.find(params[:user_id])
+    
 end
 
 def create 
-    @tracked_meal = TrackedMeal.new
-    @user = User.find(params[:user_id])
+    @tracked_meal.create(form_params)
 end
 
 def show
     @tracked_meals = TrackedMeal.all
 end
+
+private
+    def form_params
+        params.require(:tracked_meal).permit(:available_meal_id, :start_time)
+    
+    end
 
 
 end
