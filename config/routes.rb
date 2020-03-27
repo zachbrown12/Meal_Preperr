eb70f1f54764cc6ddb29a48a1994ca83cff8ae01
anoorzi <anoorzi123@gmail.com>
2020-03-26 23:12:41 -0400

    css stuff

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :available_meals
   end
   resources :ingredients
+  resources :home, only: [:index]
  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
